c69f53117c3af1290f748a5916ab94eb021bf19d
amazingasad <asadme631@gmail.com>
2021-09-23 20:59:06 +0600

    Added javascript & some profile info

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginButton = document.getElementById("login-button");
+loginButton.addEventListener("click", function(){
+    const loginForm = document.getElementById("container");
+    loginForm.style.display = "none"
+})

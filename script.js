58bc21185459a3847876323f711e6283dd8906ef
amazingasad <asadme631@gmail.com>
2021-09-25 11:34:42 +0600

    Added some media query,  email & password

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const loginButton = document.getElementById("login-button");
 loginButton.addEventListener("click", function(){
+    const profile = document.getElementById("hide");
     const loginForm = document.getElementById("container");
-    loginForm.style.display = "none"
+    const emailInfo = document.getElementById("email").value;
+    const passwordInfo = document.getElementById("password").value;
+    if(emailInfo == "me@in-asad.com" && passwordInfo == "also"){
+        profile.style.display = "block";
+        loginForm.style.display = "none";
+        alert("Login Successful")
+        console.log("right")
+    }
+    else{
+        alert("Invalid Password");
+    }
 })
